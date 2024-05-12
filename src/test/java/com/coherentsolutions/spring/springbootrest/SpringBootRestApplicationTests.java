21cdda7121c83c8ebd1ff465ea76c09fa8db512c
iWaraxe <121664848+MariaESLIW@users.noreply.github.com>
2024-05-13 00:29:23 +0200

    Initial commit

---
+++ src/test/java/com/coherentsolutions/spring/springbootrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.spring.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
